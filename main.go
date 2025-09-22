4accc78812b5fe5d7d4f516b0f88467c2c59cead
mohdanas96 <mohammedanasdk963@gmail.com>
2025-09-22 15:40:42 +0530

    change main function to startRepl()

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	sc := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("> Pokedex ")
-		sc.Scan()
-		inputs := sc.Text()
-		inputsSlice := cleanInput(inputs)
-		fmt.Printf("Your command was: %s\n", inputsSlice[0])
-	}
-}
-
-func cleanInput(text string) []string {
-	sliceOfTexts := strings.Split(strings.ToLower(strings.TrimSpace(text)), " ")
-	return sliceOfTexts
+	startRepl()
 }
